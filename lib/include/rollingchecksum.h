2ce574163cd4ea37216c31996e70904275a400d5
palucki <piotrek.palucki@gmail.com>
2022-07-12 23:04:27 +0200

    RollingChecksum docs updated

--- lib/include/rollingchecksum.h
+++ lib/include/rollingchecksum.h
@@ -3,8 +3,9 @@
 #include <vector>
 
 /*!
- * \brief The FastSignature class implements the hash function based on Adler32 checksum.
+ * \brief The RollingChecksum class implements the hash function based on Adler32 checksum.
  * See https://www.samba.org/~tridge/phd_thesis.pdf and https://rsync.samba.org/tech_report/node3.html
+ * The checksum is 32-bit long. It is used as a filter for more expensive operations.
  */
 
 class RollingChecksum
@@ -12,8 +13,10 @@ class RollingChecksum
 public:
     RollingChecksum(const std::vector<unsigned char>& data, const int factor);
 
+    //! Recomputes the checksum discarding outgoing and including incoming bytes, returns computed checksum
     long long roll(const unsigned char outgoing, const unsigned char incoming);
 
+    //! Getter for current checksum value
     long long value();
 private:
     const int m_block_size;
